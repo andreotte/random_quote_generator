848fc3c1bdf6f35c6acdf5c3c6b95419bd88750c
Andre Otte <andre.b.otte1@gmail.com>
2019-01-31 21:55:44 -0500

    Improvements and additions
    -Added 10 sec auto refresh to HTML doc, generating new quote.
    -Set the background to be a random color.
    -Added categorization tags.
    -Added comments.

--- js/quotes.js
+++ js/quotes.js
@@ -1,33 +1,42 @@
+// Document to hold quotes.
+
 var quotes = [
   {
     source: 'Mark Andreessen',
     quote: "Everything around you that you call 'life' was made up by people that were no smarter than you.",
     citation: 'Netscape co-founder',
-    year: '2000'
+    year: '2000',
+    category: 'business'
   },
   {
     source: 'Seth Godin',
     quote: "So the goal isn't to get good ideas; the goal is to get bad ideas. Because once you get enough bad ideas, then some good ones have to show up.",
-    citation: 'Author/Marketing Wizard'
+    citation: 'Author/Marketing Wizard',
+    category: 'business'
   },
   {
     source: 'Christopher Sommer',
-    quote: "When in doubt, work on the deficiencies you're most embarrased by."
+    quote: "When in doubt, work on the deficiencies you're most embarrased by.",
+    category: 'life-hacking'
   },
   {
     source: 'Christopher Sommer',
-    quote: "Slow down. Where's the fire?"
+    quote: "Slow down. Where's the fire?",
+    category: 'life-hacking'
   },
   {
     source: "Joe De Sena",
-    quote: "It could always be worse."
+    quote: "It could always be worse.",
+    category: 'Stoicism'
   },
   {
     source: 'James Cameron',
-    quote: "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success."
+    quote: "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success.",
+    category: 'business'
   },
   {
     source: 'Colonel David Hackworth',
-    quote: "If you find yourself in a fair fight, you didn't plan your mission property."
+    quote: "If you find yourself in a fair fight, you didn't plan your mission property.",
+    category: 'business'
   }
 ];
--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
 /******************************************
 Treehouse Techdegree:
 FSJS project 1 - A Random Quote Generator
+By Andre Otte
 ******************************************/
-
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
-
-
+// Script fail
 
 // Generates random quote
 function getRandomQuote(arr) {
@@ -14,24 +12,34 @@ function getRandomQuote(arr) {
 }
 
 // Sends string to html file to print to page
-function showQuote () {
+function printQuote () {
   var randomQuote = getRandomQuote(quotes);
   var html = '<p class="quote">' + randomQuote.quote + '</p>' + '<p class="source">' + randomQuote.source;
 
-  // Optional object property 'citation'
+  // Optional 'citation' property to add to string
   if ('citation' in randomQuote) {
     html += '<span class="citation">' + randomQuote.citation + '</span>';
   }
 
-  // Optional object property 'year'
+  // Optional 'year' property to add to string
   if ('year' in randomQuote) {
     html += '<span class="year">' + randomQuote.year + '</span></p>';
   }
 
+  // Optional 'category' property to add to the string
+  if ('category' in randomQuote) {
+    html += '<p class="category">' + randomQuote.category +  '</p>';
+  }
+
   document.getElementById("quote-box").innerHTML = html;
+
+  // Set random background color - random hex generator from css-tricks.com
+  // https://css-tricks.com/snippets/javascript/random-hex-color/
+  var randomColor = Math.floor(Math.random()*16777215).toString(16);
+  document.body.style.backgroundColor = '#' + randomColor;
 }
 
-showQuote();
+printQuote();
 
 // Button to show another quote
-document.getElementById('loadQuote').addEventListener("click", showQuote, false);
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
